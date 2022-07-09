9f01dfb82298928d842c4da17a87157f0d21e049
Flechashe <se.amigos@gmail.com>
2022-07-08 22:24:41 -0300

    Clase

--- modules/mercadoPago.js
+++ modules/mercadoPago.js
@@ -1,18 +1,38 @@
 // Step 7
+const mercadoPago = require('mercadopago');
+const credential =
+    process.env.MP ||
+    'TEST-3645381466893064-070611-351447983b87f23645ba15f728ce6668-157889446';
+let server = process.env.SERVER || 'http://localhost:3030';
+const feedback = `${server}/checkout/feedback`;
 
-// const ??? = require(???)
-// const credential = ??? 
-// let server = ???
-// const success = ???
-// const failure = ???
-// const pending = ???
-/*
-const mp  = async (???,???,???) => {
+const mp = async (items, installments, shipping) => {
     try {
         // Magic
+        mercadoPago.configure({
+            access_token: credential,
+        });
+        let config = {
+            items: items,
+            back_urls: {
+                success: feedback,
+                failure: feedback,
+                pending: feedback,
+            },
+            payment_methods: {
+                installments: installments,
+            },
+            auto_return: 'approved',
+            shipments: {
+                cost: shipping,
+                mode: 'not_specified',
+            },
+        };
+
+        let preferences = await mercadoPago.preferences.create(config);
+        return preferences;
     } catch (error) {
-        throw new Error(error)
+        throw new Error(error);
     }
-}
-module.exports = mp
-*/
+};
+module.exports = mp;
